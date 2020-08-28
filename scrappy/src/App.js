274d30ca4075f9011d172f2daf45fc79cccb475d
chelsejw <chelsejw@gmail.com>
2020-08-28 22:46:45 +0800

    show loading text while loading data

--- scrappy/src/App.js
+++ scrappy/src/App.js
@@ -4,16 +4,12 @@ import Nav from './components/Nav'
 
 import Table from './components/Table'
 
-const App = (props) => {
+const App = () => {
     const [data, setData] = useState([]);
-
     useEffect(() => {
-
-
         axios.get("/jobs").then((res) => {
             setData(res.data.response)
         }).catch(err => console.log(`Error in axios request`, err))
-
     }, []);
 
     useEffect(()=> {
@@ -25,8 +21,7 @@ const App = (props) => {
       <div>
         <Nav />
         <div className="container">
-
-          <Table data={data} />
+          {data.length > 1 ? <Table data={data} /> : <div className="text-center my-5 mx-0"><h2>Loading...</h2></div>}
         </div>
       </div>
     );
