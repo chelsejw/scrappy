d1f4f8fa7080ad6decb3746a779f6c552fdee93f
chelsejw <chelsejw@gmail.com>
2020-08-30 15:54:31 +0800

    divide components

--- scrappy/src/App.js
+++ scrappy/src/App.js
@@ -3,9 +3,8 @@ import axios from "axios";
 import Nav from './components/Nav'
 import RingLoader from 'react-spinners/RingLoader'
 import Table from './components/Table'
-import StackBtn from './components/StackBtn'
 import moment from 'moment'
-
+import SupportedHeader from './components/SupportedHeader'
 const App = () => {
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true)
@@ -13,12 +12,16 @@ const App = () => {
     const [lastUpdate, setLastUpdate] = useState({})
     const [stack, setStack] = useState([])
     useEffect(() => {
-        axios.get("/jobs")
+        axios.get("/hello")
         .then((res) => {
+            console.log(res.data)
             setJobs(res.data.response.jobs)
-            setLastUpdate(prev => {
-              return {...prev, jobs: res.data.response.last_update}
-            })
+            setLastUpdate((prev) => {
+              return {
+                ...prev,
+                jobs: res.data.response.jobs[0].created_at,
+              };
+            });
             setLoading(false)
         })
         .catch((err) => {
@@ -29,9 +32,9 @@ const App = () => {
 
         axios.get("/tech")
         .then((res) => {
-            setStack(res.data.response.tech)
+            setStack(res.data)
             setLastUpdate(prev => {
-              return {...prev, tech: res.data.response.last_update}
+              return {...prev, tech: res.data[res.data.length-1].created_at}
             })
         })
         .catch((err) => {
@@ -45,18 +48,13 @@ const App = () => {
         console.log(lastUpdate)
     }, [jobs, stack, lastUpdate])
 
-    const stackItems = stack.map(item => {
-      return <StackBtn name={item}/>
-    })
-
     return (
       <div>
         <Nav />
         <div className="container">
 
-          <h1 className="mt-3 text-center main-header">Tech@SG</h1>
-          <div className="text-center">Supported Technologies as of {moment(lastUpdate.tech).format("DD MMMM YYYY")}</div>
-          <div className="my-3 w-75 mx-auto text-center">{stackItems}</div>
+        <SupportedHeader lastUpdate={moment(lastUpdate.tech).format("DD MMMM YYYY")} stack={stack}/>
+
           {!failure && !loading && 
             (<div>
               <div className="text-center">Last added jobs: {moment(lastUpdate.jobs).format("DD MMM, h:mm:ss a")}</div>
